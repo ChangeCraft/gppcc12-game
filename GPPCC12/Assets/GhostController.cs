7fe5d3b977ce79e1de08256bd34d49f0086498ed
ChangeCraft <changer07lp@gmail.com>
2018-03-06 19:38:12 +0100

    Fixed a Bug with switching Ghosts; Added a Tag for the controlled ghost
    Added a Tag which appears above the ghost, which is currently under control. This way the ghost-player can see who he controlls.
    
    Fixed a bug, where the switch ghosts "method" (currently the whole update function) would be called every frame [SPACE]is being held down. This would cause a very random ghost switching. Now the function only gets called on the frame the player presses DOWN [SPACE].

--- GPPCC12/Assets/GhostController.cs
+++ GPPCC12/Assets/GhostController.cs
@@ -7,6 +7,9 @@ public class GhostController : MonoBehaviour {
     [SerializeField]
     private GameObject[] ghosts;
 
+    [SerializeField]
+    private GameObject playerTagSprite;
+
     private int currentControlledGhost = 0;
 
     private void Start()
@@ -28,33 +31,68 @@ public class GhostController : MonoBehaviour {
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        //On Space we want to switch to the next ghost
+        if(Input.GetKeyDown(KeyCode.Space))
         {
+            //Increments to next ghost because we want to switch to him
             currentControlledGhost++;
 
+            //If we where to overflow the arraylenght reset to 0
             if (currentControlledGhost > ghosts.Length)
                 currentControlledGhost = 0;
 
-            SwitchControlledGhostTo(currentControlledGhost);
+            //Switch controlls to the given ghost
+            //if 0 no ghost can be controlled
+            SwitchControlledGhost(currentControlledGhost);
         }
     }
 
-    private void SwitchControlledGhostTo (int ghostNumber)
+    private void SwitchControlledGhost (int _ghostNumber)
     {
+        //Sets all ghosts to not player controlled
         foreach (var ghost in ghosts)
         {
+            RemovePlayerTag(ghost);
             GhostMove ghostMove = ghost.GetComponent<GhostMove>();
             ghostMove.SetControlledTo(false);
         }
 
-        if (ghostNumber != 0)
+        //Sets the given ghost to be player controlled if ghostNumber
+        // is not 0, because 0 means no ghost is player controlled
+        if (_ghostNumber != 0)
         {
             //Subtraction done becaues the first array value has an index of 0
             //and 0 is already preserved for "no ghost"
-            ghostNumber--;
+            _ghostNumber--;
 
-            ghosts[ghostNumber].GetComponent<GhostMove>().SetControlledTo(true);
+            SetPlayerTag(_ghostNumber);
+            ghosts[_ghostNumber].GetComponent<GhostMove>().SetControlledTo(true);
         }
     }
 
+    //Instantiates a player tag and adds it as a child to the
+    //given ghost index in the ghost array
+    private void SetPlayerTag (int _ghostNumber)
+    {
+        Transform ghostTransform = ghosts[_ghostNumber].transform;
+
+        //Instantiate a playerTag
+        GameObject playerTag = Instantiate(playerTagSprite, ghostTransform);
+        playerTag.transform.parent = ghostTransform;
+    }
+
+    //Removes All childs form the given Object that have the 
+    //same tag as a PlayerTag
+    private void RemovePlayerTag (GameObject _ghost)
+    {
+        //cycle through all childs of the given ghost 
+        for (int i = 0; i < _ghost.transform.childCount; i++)
+        {
+            Transform child = _ghost.transform.GetChild(i);
+            
+            //Destroys the child if it is a playertag
+            if (child.tag == playerTagSprite.tag)
+                Destroy(child.gameObject);
+        }
+    }
 }
