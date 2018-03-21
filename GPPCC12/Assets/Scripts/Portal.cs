7d8452c6e63ffeaa90f3c994bcaec596e2ed74cc
ChangeCraft <changer07lp@gmail.com>
2018-03-21 08:47:50 +0100

    Added PortalPrefab with Animation; Removed old soundfiles; Moved Dots script; Added dot respawn logic (portal)

--- GPPCC12/Assets/Scripts/Portal.cs
+++ GPPCC12/Assets/Scripts/Portal.cs
@@ -8,12 +8,21 @@ public class Portal : MonoBehaviour {
     private Vector2 teleportPosition;
     private string playerTag = "Player";
 
+    [SerializeField]
+    private GameObject dotsPrefab;
+
+    [SerializeField]
+    private AudioClip sound;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == playerTag)
         {
             other.transform.position = teleportPosition;
             other.GetComponent<PacmanMove>().SetDestinationToPosition();
+            Instantiate(dotsPrefab);
+            AudioSource.PlayClipAtPoint(sound, new Vector3(0, 0, 0));
+            Destroy(gameObject);
         }
     }
 }
